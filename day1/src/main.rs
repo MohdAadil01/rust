f9663913f6dc9bc9beae8a9822165d6ba858593f
MohdAadil01 <mohdaadil4240@gmail.com>
2025-01-19 16:16:27 +0530

    even/odd number

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,19 @@
+use core::num;
+//QUESTION:- function to print even/odd number
+use std::io::{self, Read};
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the number!");
+    let mut number = String::new();
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read the line.");
+    let num: i32 = number.trim().parse().expect("Not a valid number");
+    let ans = is_even(num);
+    println!("{}", ans);
+}
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    }
+    return false;
 }
