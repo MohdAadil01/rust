12383fe581e456f73ff0f40dd34ef59d52d5c3c1
MohdAadil01 <mohdaadil4240@gmail.com>
2025-01-19 17:30:00 +0530

    length of input string

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,44 +1,44 @@
-// use core::num;
-// //QUESTION:- function to print even/odd number
+// // use core::num;
+// // //QUESTION:- function to print even/odd number
+// // use std::io::{self, Read};
+// // fn main() {
+// //     println!("Enter the number!");
+// //     let mut number = String::new();
+// //     io::stdin()
+// //         .read_line(&mut number)
+// //         .expect("Failed to read the line.");
+// //     let num: i32 = number.trim().parse().expect("Not a valid number");
+// //     let ans = is_even(num);
+// //     println!("{}", ans);
+// // }
+
+// // fn is_even(num: i32) -> bool {
+// //     if num % 2 == 0 {
+// //         return true;
+// //     }
+// //     return false;
+// // }
+
+// // !QUESTION:- fibbonacci number
 // use std::io::{self, Read};
 // fn main() {
-//     println!("Enter the number!");
-//     let mut number = String::new();
+//     println!("Enter number!");
+//     let mut input: String = String::new();
 //     io::stdin()
-//         .read_line(&mut number)
-//         .expect("Failed to read the line.");
-//     let num: i32 = number.trim().parse().expect("Not a valid number");
-//     let ans = is_even(num);
-//     println!("{}", ans);
-// }
+//         .read_line(&mut input)
+//         .expect("Failed to read the line");
 
-// fn is_even(num: i32) -> bool {
-//     if num % 2 == 0 {
-//         return true;
+//     let num: i32 = input.trim().parse().expect("Not a valid input");
+
+//     let ans: i32 = func(num);
+//     println!("Fibonacci of number {} is {}", num, ans);
+// }
+// fn func(num: i32) -> i32 {
+//     if num <= 1 {
+//         return num;
 //     }
-//     return false;
+//     return func(num - 1) + func(num - 2);
 // }
-
-// !QUESTION:- fibbonacci number
-use std::io::{self, Read};
-fn main() {
-    println!("Enter number!");
-    let mut input: String = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read the line");
-
-    let num: i32 = input.trim().parse().expect("Not a valid input");
-
-    let ans: i32 = func(num);
-    println!("Fibonacci of number {} is {}", num, ans);
-}
-fn func(num: i32) -> i32 {
-    if num <= 1 {
-        return num;
-    }
-    return func(num - 1) + func(num - 2);
-}
 // fn func(num: i32) -> i32 {
 //     let mut first: i32 = 0;
 //     let mut second: i32 = 1;
@@ -52,3 +52,19 @@ fn func(num: i32) -> i32 {
 //     }
 //     return second;
 // }
+
+use std::io;
+
+// !QUESTION 3:- function to return string length
+fn main() {
+    println!("Enter the string. ");
+    let mut str: String = String::new();
+    io::stdin()
+        .read_line(&mut str)
+        .expect("Unable to read input from the line.");
+    let ans: usize = get_length(str);
+    println!("The size of the input string is {}", ans);
+}
+fn get_length(str: String) -> usize {
+    return str.trim().chars().count();
+}
