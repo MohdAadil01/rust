1725cc4f1ca09b24809f973ccb69e2a2ae83f049
MohdAadil01 <mohdaadil4240@gmail.com>
2025-01-20 15:18:41 +0530

    ownershipt

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,8 +1,22 @@
-// MEMORY MANAGEMENT AND MUTABILITY
+// // MEMORY MANAGEMENT AND MUTABILITY
+// fn main() {
+//     let mut str: String = String::from("Hi there");
+//     // the str is stored in stack, however the "hi there" is stored in heap as it is dynamic
+//     str.insert(6, 'c');
+//     // all variables are immutable by default in rust;
+//     println!("{}", str);
+// }
+
+// !OWNERSHIP
+// every value in rust has an owner
+// there can be only one owner at a time
+// if the owner goes out of scope, the value gets dropped
+
 fn main() {
-    let mut str: String = String::from("Hi there");
-    // the str is stored in stack, however the "hi there" is stored in heap as it is dynamic
-    str.insert(6, 'c');
-    // all variables are immutable by default in rust;
-    println!("{}", str);
+    let s1: String = String::from("This is the string");
+    // ?THE OWNER OF STRING IS s1 here
+    let s2: String = s1;
+    //?HERE OWNER OF STRING IS s2 now, so if i try to access s2 here, i will be able to access, however if i try to access s1, i won't be able to acces;
+    println!("{}", s2);
+    println!("{}", s1);
 }
