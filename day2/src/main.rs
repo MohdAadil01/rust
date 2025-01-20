b4e252aee42114b8de44a3d40b5c5e110674920e
MohdAadil01 <mohdaadil4240@gmail.com>
2025-01-20 15:09:08 +0530

    mutability

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,8 @@
+// MEMORY MANAGEMENT AND MUTABILITY
 fn main() {
     let mut str: String = String::from("Hi there");
     // the str is stored in stack, however the "hi there" is stored in heap as it is dynamic
     str.insert(6, 'c');
+    // all variables are immutable by default in rust;
     println!("{}", str);
 }
